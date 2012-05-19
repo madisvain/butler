cf132bf36e5c25224c1a004cececd612e34953bf
Madis Väin <madisvain@gmail.com>
2012-05-19 22:51:18 +0300

    rewrote messaging system and added some templates

--- fabfile.py
+++ fabfile.py
@@ -1,17 +1,19 @@
 #coding: utf-8
 
-from time import time, localtime, strftime
+from time import time, localtime
 
-from fabric.api import env, run
-from fabric.colors import green, red
+from fabric.api import env, run, sudo, local, put
+from fabric.utils import puts
+from fabric.colors import blue, cyan, green, magenta, red, white, yellow
 
-from utils import make_password, delta
+from utils import make_password, delta, message
 
 
 # Settings
-env.hosts = ['ec2-46-137-36-192.eu-west-1.compute.amazonaws.com']
+env.hosts = ['ec2-46-137-58-134.eu-west-1.compute.amazonaws.com']
 env.user = 'ec2-user'
 env.key_filename = '~/.ssh/aws.pem'
+env.start_time = time()
 
 
 """
@@ -27,27 +29,25 @@ def install():
     """
     Installs the required software required for serving the application
     """
-    install_requirements()
-    install_http_server()
-    install_cache()
-    install_wsgi_server()
-    install_db()
+    install_pip()
+    install_nginx()
+    install_memcached()
+    install_uwsgi()
+    install_mysql()
 
 def build():
     """"
     Builds the complete environment and deployes the application into it.
     """
     
-    start_time = time()
-    print('time' + '\t\t' + 'delta' + '\t' + 'message')
-    print(strftime("%H:%M:%S", localtime()) + '\t' + delta(start_time) + '\t' + green('Starting build process...'))
+    message('Starting build process ...', color=green)
     
-    #update()
-    #install()
+    update()
+    install()
     #virtualenv()
     #deploy()
 
-    print(strftime("%H:%M:%S", localtime()) + '\t' + delta(start_time) + '\t' + green('Your build is complete sir!'))
+    message('Your build is complete!', color=green)
 
 """
 Commands
@@ -55,31 +55,54 @@ Commands
 
 # System commands
 def update():
-    print("Udating system software")
+    message('Udating system software ...', color=yellow)
     sudo('yum -y update')
 
 def free_memory():
+    message('Outputting free memory:', color=yellow)
     run('free -m')
 
-# Software installation commands
-def install_requirements():
-    sudo('easy_install pip')
-    sudo('yum -y install gcc-c++')
-    sudo('yum -y install python-devel')
-    sudo('yum -y install libxml2-python libxml2-devel')
+def restart_stack():
+    message('Restarting everything', color=yellow)
+    sudo('service nginx restart')
+    sudo('service mysqld restart')
+    # stopping uwsgi deamons
+    #kill -INT `cat /tmp/uwsgi.pid`
 
-def install_http_server():
+def reload_stack():
+    message('Reloading everything ...', color=yellow)
+    sudo('service nginx reload')
+    sudo('service mysqld restart')
+
+
+# Software installation commands
+def install_pip():
+    message('Installing PIP for python ...', color=yellow)
+    sudo('easy_install pip')
+
+def install_nginx():
+    message('Installing NGINX ...', color=yellow)
     sudo('yum -y install nginx')
     sudo('service nginx start')
     sudo('chkconfig nginx on')
+    #returns number of cores
+    #cat /proc/cpuinfo | grep processor | wc -l
 
-def install_cache():
+def install_memcached():
+    message('Installing Memcached ...', color=yellow)
     sudo('yum -y install memcached')
 
-def install_wsgi_server():
+def install_uwsgi():
+    message('Installing uWSGI ...', color=yellow)
+    # requirements for uWSGI
+    sudo('yum -y install gcc-c++')
+    sudo('yum -y install python-devel')
+    sudo('yum -y install libxml2-python libxml2-devel')
+    # install uWSGI
     sudo('pip install uwsgi')
 
-def install_db():
+def install_mysql():
+    message('Installing & configuring MySQL ...', color=yellow)
     password = make_password(10)
     
     sudo('yum -y install mysql mysql-server MySQL-python')
@@ -95,7 +118,6 @@ def install_db():
     # Write the output file
     
 
-
 # Environment setup commands
 def python_requirements():
     run('pip install -r templates/requirements.txt')
@@ -105,3 +127,11 @@ def virtualenv():
     
 def repository():
     pass
+
+def upload_tar_from_git():
+    message('Creating an archive from the current Git master branch and uploading it ..', color=yellow)
+    local('git archive --format=tar master | gzip > release.tar.gz')
+    sudo('mkdir /srv/release && chown ec2-user /srv/release/')
+    put('release.tar.gz', '/srv/release/')
+    run('cd /srv/release/ && tar zxf release.tar.gz && rm release.tar.gz')
+    local('rm release.tar.gz')
--- utils.py
+++ utils.py
@@ -2,8 +2,10 @@
 
 import random
 import string
+from time import time, localtime, strftime
 
-from time import time
+from fabric.api import env
+from fabric.colors import white
 
 
 ''' A random password generator
@@ -11,6 +13,13 @@ from time import time
 def make_password(length=10, chars=string.ascii_letters + string.digits):
 	return ''.join(random.choice(chars) for x in range(length))
 
-
+''' Calculates a delta from the start_time
+'''
 def delta(start_time):
     return str(round((time() - start_time), 2))
+
+''' Prints a timed message
+'''
+def message(message, start_time=time(), color=white):
+    d = delta(env.start_time)
+    print(strftime("%H:%M:%S", localtime()) + '\t' + d + '\t' + color(message))
