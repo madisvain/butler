f6797b0430759c64ade40b64b2121a746f3c47fb
Madis Väin <madisvain@gmail.com>
2012-05-19 13:58:32 +0300

    some butler updates added memcached

--- fabfile.py
+++ fabfile.py
@@ -1,37 +1,57 @@
 #coding: utf-8
 
-import time
+from time import time, localtime, strftime
 
 from fabric.api import env, run
+from fabric.colors import green, red
 
-from utils import make_password
+from utils import make_password, delta
 
 
 # Settings
 env.hosts = ['ec2-46-137-36-192.eu-west-1.compute.amazonaws.com']
 env.user = 'ec2-user'
 env.key_filename = '~/.ssh/aws.pem'
-#env.password = None
 
 
-'''
+"""
 Master commands
-'''
+"""
+def deploy():
+    """
+    Deployes the application.
+    """
+    upload_repository()
+
 def install():
+    """
+    Installs the required software required for serving the application
+    """
     install_requirements()
     install_http_server()
+    install_cache()
     install_wsgi_server()
     install_db()
 
 def build():
-    update()
-    install()
-    virtualenv()
+    """"
+    Builds the complete environment and deployes the application into it.
+    """
+    
+    start_time = time()
+    print('time' + '\t\t' + 'delta' + '\t' + 'message')
+    print(strftime("%H:%M:%S", localtime()) + '\t' + delta(start_time) + '\t' + green('Starting build process...'))
+    
+    #update()
+    #install()
+    #virtualenv()
+    #deploy()
 
+    print(strftime("%H:%M:%S", localtime()) + '\t' + delta(start_time) + '\t' + green('Your build is complete sir!'))
 
-'''
+"""
 Commands
-'''
+"""
 
 # System commands
 def update():
@@ -51,6 +71,10 @@ def install_requirements():
 def install_http_server():
     sudo('yum -y install nginx')
     sudo('service nginx start')
+    sudo('chkconfig nginx on')
+
+def install_cache():
+    sudo('yum -y install memcached')
 
 def install_wsgi_server():
     sudo('pip install uwsgi')
@@ -58,15 +82,19 @@ def install_wsgi_server():
 def install_db():
     password = make_password(10)
     
-    sudo('yum -y install mysql-server mysql mysql-devel')
+    sudo('yum -y install mysql mysql-server MySQL-python')
     sudo('service mysqld start')
+    sudo('chkconfig mysqld on')
     
     # Scure the mysql installation
     run('mysqladmin -u root password ' + password)
     run('mysql -uroot -p' + password + ' -e \"DROP USER \'\'@\'localhost\';\"')
     run('mysql -uroot -p' + password + ' -e \"DROP DATABASE test;\"')
     run('mysql -uroot -p' + password + ' -e \"FLUSH PRIVILEGES;\"')
-    #run('mysql_secure_installation') # Secure the mysql installation
+
+    # Write the output file
+    
+
 
 # Environment setup commands
 def python_requirements():
--- utils.py
+++ utils.py
@@ -3,7 +3,14 @@
 import random
 import string
 
+from time import time
+
+
 ''' A random password generator
 '''
 def make_password(length=10, chars=string.ascii_letters + string.digits):
 	return ''.join(random.choice(chars) for x in range(length))
+
+
+def delta(start_time):
+    return str(round((time() - start_time), 2))
